d5a16d44540253aa715dc8d9e4db4d88dc163263
viih_estheffany <vitoriagodoi@gmail.com>
2023-08-16 10:37:10 -0300

    Configurado banco MySQL

--- webapp/settings.py
+++ webapp/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306', 
+        }
+    
 }
 
 
